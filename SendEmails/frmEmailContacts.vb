e2c74edb5576b19b19e31d17ec72790002483b65
vighneshtrivedi2004 <vighneshtrivedi2004@gmail.com>
2017-07-20 22:03:40 +0530

    Feedback Changes
    Feedback Changes

--- SendEmails/frmEmailContacts.vb
+++ SendEmails/frmEmailContacts.vb
@@ -24,14 +24,6 @@ Public Class frmEmailContacts
         InitializeComponent()
         strAccount = Account
     End Sub
-
-    Public Function IsValidEmailAddress() As Boolean
-        ' Return true if strIn is in valid e-mail format.
-        Return Regex.IsMatch(txtEmail.Text,
-                   "^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
-                   "(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
-                   RegexOptions.IgnoreCase)
-    End Function
 #End Region
 
 #Region "Form and Control Events"
